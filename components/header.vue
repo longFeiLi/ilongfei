693dd04057daffbf9f9a121d7470945ab773c8a3
longfei <longfeili0925@gmail.com>
2020-03-22 01:45:25 +0800

    ilongfei v1.0

---
+++ components/header.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="header">
+    <div class="logo">iLongfei</div>
+    <div class="slide-right">
+      <div class="list">
+        <div
+          :class="{ active: index === navIndex }"
+          :key="index"
+          class="item"
+          v-for="(item, index) in navList"
+        >
+          <a :href="item.url">{{ item.name }}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  components: {},
+  data() {
+    return {
+      navList: [
+        {
+          name: '首页',
+          url: ''
+        },
+        {
+          name: '技术类',
+          url: ''
+        },
+        {
+          name: '书籍类',
+          url: ''
+        },
+        {
+          name: '娱乐类',
+          url: ''
+        },
+        {
+          name: '其他',
+          url: ''
+        }
+      ],
+      navIndex: 0 //默认选中第一个
+    };
+  },
+  created() {}
+});
+</script>
+
+<style lang="scss">
+// 定义颜色
+$primary-color: #1890ff;
+.header {
+  width: 100%;
+  height: 60px;
+  display: flex;
+  background: #fff;
+  box-shadow: 0 2px 8px #f0f1f2;
+  position: relative;
+  z-index: 10;
+  max-width: 100%;
+  .logo {
+    width: 20%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 30px;
+    text-transform: capitalize;
+    cursor: pointer;
+  }
+  .slide-right {
+    width: 80%;
+    display: block;
+    color: $primary-color;
+    text-align: right;
+    .list {
+      margin-right: 20%;
+      height: 60px;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      .item {
+        margin-left: 30px;
+        padding: 0px 20px;
+        text-align: center;
+        font-size: 14px;
+        line-height: 60px;
+        a {
+          color: rgba(0, 0, 0, 0.65);
+          &:hover {
+            color: $primary-color;
+          }
+        }
+      }
+      .active {
+        border-bottom: 2px solid $primary-color;
+        a {
+          color: $primary-color;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <i-header />
+    <!-- 引入头部组件 -->
+    <nuxt />
+    <!-- 底部 -->
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import iHeader from '../components/header.vue'
+export default Vue.extend({
+  components: {
+    iHeader
+  },
+  data() {
+    return {
+      index: 1
+    }
+  }
+});
+</script>
+
+<style>
+html {
+  font-family: Microsoft YaHei, Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+p {
+  margin: 0px;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
